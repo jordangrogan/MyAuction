de9005926a95f94f0a0e58cd0e46c6ba8808a854
Jordan Grogan <jordangrogan11@gmail.com>
2018-11-15 17:40:04 -0500

    Insert - skeleton and ourSysDATE insert

--- insert.sql
+++ insert.sql
@@ -1,1 +1,21 @@
+-- My Auction
+-- Team 1
+-- Sample Data
+-- Jordan Grogan, John Wartonick, Wyatt Bobis
 
+-- 1 ourSysDATE
+INSERT INTO ourSysDATE (C_DATE) VALUES (TO_DATE('2018-11-15 17:38:39', 'YYYY-MM-DD HH24:MI:SS'));
+
+-- 3 Customer
+
+-- 1 Administrator
+
+-- 5 Product
+
+-- 10 Bidlog
+
+-- 5 Category
+
+-- 5 BelongsTo
+
+commit;
