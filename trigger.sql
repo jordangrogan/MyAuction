3880b35e12649056d9c7c000531a0f552b6138fa
Jordan Grogan <jordangrogan11@gmail.com>
2018-11-15 18:03:46 -0500

    Trigger - skeleton

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,27 @@
+-- My Auction
+-- Team 1
+-- Triggers, Functions, Procedures
+-- Jordan Grogan, John Wartonick, Wyatt Bobis
 
+-- proc_putProduct
+
+
+-- constraint: the amount of the new bid on a product > the currently highest bid (i.e. "amount" attribute) on that product
+
+
+-- trig_bidTimeUpdate
+
+
+-- trig_updateHighBid
+
+
+-- func_productCount(x, c)
+
+
+-- func_bidCount(x, u)
+
+
+-- func_buyingAmount(x, u)
+
+
+-- trig_cloudAuctions
