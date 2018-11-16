6cd27543ac65089e082a6e124b3e9d3c13a3160a
Jordan Grogan <jordangrogan11@gmail.com>
2018-11-16 15:56:55 -0500

    proc_putProduct start

--- trigger.sql
+++ trigger.sql
@@ -4,6 +4,21 @@
 -- Jordan Grogan, John Wartonick, Wyatt Bobis
 
 -- proc_putProduct
+-- The procedure takes as inputs all the
+--valid information that the user provides, infers other information (i.e., auction id and
+--starting date), and records all information to the database.
+CREATE OR REPLACE PROCEDURE proc_putProduct (product_name in varchar2, product_description in varchar2, seller_id in number, category in varchar2, min_price in number, num_days in number) AS
+auction_id number;
+current_date date;
+BEGIN
+    SELECT MAX(auction_id)+1 INTO auction_id FROM product;
+    SELECT c_date INTO current_date FROM oursysdate;
+    
+    INSERT INTO product (AUCTION_ID, NAME, DESCRIPTION, SELLER, START_DATE, MIN_PRICE, NUMBER_OF_DAYS, STATUS) VALUES 
+        (auction_id, product_name, product_description, seller_id, current_date, min_price, num_days, 'under auction');
+    INSERT INTO belongsto (AUCTION_ID, CATEGORY) VALUES (auction_id, category);
+END;
+/
 
 
 -- constraint: the amount of the new bid on a product > the currently highest bid (i.e. "amount" attribute) on that product
