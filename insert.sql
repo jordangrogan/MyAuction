b2bc704c5a4aae89d9b1ea4fa9ded9bd59510724
Jordan Grogan <jordangrogan11@gmail.com>
2018-11-15 19:23:56 -0500

    Customer sample data

--- insert.sql
+++ insert.sql
@@ -7,9 +7,12 @@
 INSERT INTO ourSysDATE (C_DATE) VALUES (TO_DATE('2018-11-15 17:38:39', 'YYYY-MM-DD HH24:MI:SS'));
 
 -- 3 Customer
+INSERT INTO Customer (LOGIN, PASSWORD, NAME, ADDRESS, EMAIL) VALUES ('jog89', 'mypass', 'Jordan Grogan', '4200 Fifth Ave, Pgh, PA 15260', 'jog89@pitt.edu');
+INSERT INTO Customer (LOGIN, PASSWORD, NAME, ADDRESS, EMAIL) VALUES ('jww36', 'mypass', 'John Wartonick', '4200 Fifth Ave, Pgh, PA 15260', 'jww36@pitt.edu');
+INSERT INTO Customer (LOGIN, PASSWORD, NAME, ADDRESS, EMAIL) VALUES ('wjb39', 'mypass', 'Wyatt Bobis', '4200 Fifth Ave, Pgh, PA 15260', 'wjb39@pitt.edu');
 
 -- 1 Administrator
-INSERT INTO Administrator (LOGIN, PASSWORD, NAME) VALUES ('admin', 'root', 'Administrator');
+INSERT INTO Administrator (LOGIN, PASSWORD, NAME, ADDRESS, EMAIL) VALUES ('admin', 'root', 'Administrator', '4200 Fifth Ave, Pgh, PA 15260', 'admin@pitt.edu');
 
 -- 5 Product
 
