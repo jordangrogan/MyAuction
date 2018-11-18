78e0e5ad997f525bd3080d256fb098ad7dd71efa
John Wartonick <jww36@pitt.edu>
2018-11-18 16:01:41 -0500

    Template
    Finished product and have a template for the others

--- insert.sql
+++ insert.sql
@@ -15,11 +15,33 @@ INSERT INTO Customer (LOGIN, PASSWORD, NAME, ADDRESS, EMAIL) VALUES ('wjb39', 'm
 INSERT INTO Administrator (LOGIN, PASSWORD, NAME, ADDRESS, EMAIL) VALUES ('admin', 'root', 'Administrator', '4200 Fifth Ave, Pgh, PA 15260', 'admin@pitt.edu');
 
 -- 5 Product
-
+insert into Product (auction_id, name, description, seller, start_date, min_price, number_of_days, status, buyer, sell_date, amount) Values(1, 'bike', 'a blue bike', 'Mike', TO_DATE('2018-11-18 17:40:01', 'YYYY-MM-DD HH24:MI:SS'), 40, 5, 'Sold', 'Bob', TO_DATE('2018-11-23 12:30:01', 'YYYY-MM-DD HH24:MI:SS'), 35);
+insert into Product (auction_id, name, description, seller, start_date, min_price, number_of_days, status, buyer, sell_date, amount) Values(1, 'shoes', 'runnin shoes', 'John', TO_DATE('2018-03-18 17:36:01', 'YYYY-MM-DD HH24:MI:SS'), 15, 3, 'Selling', 'Jeff', TO_DATE('2018-03-21 02:30:01', 'YYYY-MM-DD HH24:MI:SS'), 15);
+insert into Product (auction_id, name, description, seller, start_date, min_price, number_of_days, status, buyer, sell_date, amount) Values(1, 'sink', 'the kitchen sink', 'Scott', TO_DATE('2018-04-18 17:40:01', 'YYYY-MM-DD HH24:MI:SS'), 12, 6, 'Sold', 'Mark', TO_DATE('2018-04-24 04:30:01', 'YYYY-MM-DD HH24:MI:SS'), 12);
+insert into Product (auction_id, name, description, seller, start_date, min_price, number_of_days, status, buyer, sell_date, amount) Values(1, 'cup', 'very big', 'Evan', TO_DATE('2018-05-18 17:40:01', 'YYYY-MM-DD HH24:MI:SS'), 5, 2, 'Selling', 'Lexy', TO_DATE('2018-05-20 11:30:01', 'YYYY-MM-DD HH24:MI:SS'), 2);
+insert into Product (auction_id, name, description, seller, start_date, min_price, number_of_days, status, buyer, sell_date, amount) Values(1, 'ball', 'a very bouncy ball', 'Nick', TO_DATE('2018-11-18 17:40:01', 'YYYY-MM-DD HH24:MI:SS'), 3, 7, 'Sold', 'Bob', TO_DATE('2018-11-25 01:30:01', 'YYYY-MM-DD HH24:MI:SS'), 3);
 -- 10 Bidlog
-
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
+insert into Bidlog (bidsn, auction_id, bidder, bid_time, amount) Values ();
 -- 5 Category
-
+insert into Category (name, parent_category) Values ();
+insert into Category (name, parent_category) Values ();
+insert into Category (name, parent_category) Values ();
+insert into Category (name, parent_category) Values ();
+insert into Category (name, parent_category) Values ();
 -- 5 BelongsTo
+insert into BelongsTo (auction_id, category);
+insert into BelongsTo (auction_id, category);
+insert into BelongsTo (auction_id, category);
+insert into BelongsTo (auction_id, category);
+insert into BelongsTo (auction_id, category);
 
 commit;
