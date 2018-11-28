ceaeb468a159d73b0b6cfce417f7037fbe3ac33c
Jordan Grogan <jordangrogan11@gmail.com>
2018-11-28 13:17:12 -0500

    Started java file

--- MyAuction.java
+++ MyAuction.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class MyAuction {
+
+  public static void main(String args[]) {
+
+    Scanner reader = new Scanner(System.in);
+    String response;
+
+    System.out.println("Welcome to My Auction!");
+
+    do {
+      System.out.println("Are you an admin or a customer?");
+      response = reader.nextLine();
+    } while(!(response.equals("admin") || response.equals("customer")));
+
+    if(response.equals("customer")) {
+      System.out.println("Welcome Customer!");
+    } else if(response.equals("admin")) {
+      System.out.println("Welcome Admin!");
+    }
+
+  }
+
+}
