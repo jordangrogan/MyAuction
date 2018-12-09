ce78810f8c8063e70b715b230bdce562f7b16cd0
John Wartonick <jww36@pitt.edu>
2018-12-09 14:34:10 -0500

    did bid on product and registerUser

--- Driver.java
+++ Driver.java
@@ -151,6 +151,23 @@ public class Driver {
     public static void test_bidOnProduct(MyAuction myauction){
     	System.out.println("Testing bidding on a product...");
     	myauction.addBid(2, "jww36", 100);
+    	Statement statement = null;
+    	try{
+    		statement = connection.createStatement();
+    		ResultSet resultSet = statement.executeQuery("SELECT * FROM Bidlog WHERE auction_id = 2 AND bidder = 'jww36' AND amount=100");
+    		if(resultSet.next()){
+    			System.out.println("\"SELECT * FROM Bidlog WHERE auction_id = 2 AND bidder = 'jww36' AND amount=100\" successfully returned a row!");
+    		}
+    	}catch(SQLException Ex) {
+            System.out.println("Error running the queries.  Machine Error: " +
+                    Ex.toString());
+        } finally {
+            try {
+                if (statement != null) statement.close();
+            } catch (SQLException e) {
+                System.out.println("Cannot close Statement. Machine error: "+e.toString());
+            }
+        }
     	System.out.println("----------------------------------------------------------------");
     }
 
@@ -175,8 +192,25 @@ public class Driver {
     public static void test_registerUser(MyAuction myauction){
     	System.out.println("Testing registering new user...");   	
     	myauction.registerUser("Test Name", "Test address", "Test email", "Test", "password", false);
-    	myauction.registerUser("Test Name 2", "Test address 2", "Test email 2", "Test2", "password2", true);
-    	
+    	Statement statement = null;
+    	try{
+    		statement = connection.createStatement();
+    		ResultSet resultSet = statement.executeQuery("SELECT * FROM Customer WHERE login = 'Test' AND password = 'password'");
+    		if(resultSet.next()){
+    			System.out.println("\"SELECT * FROM Customer WHERE login = 'Test' AND password = 'password'\" successfully returned a row!");
+    		}
+    		statement = connection.createStatement();
+    		
+    	}catch(SQLException Ex) {
+            System.out.println("Error running the queries.  Machine Error: " +
+                    Ex.toString());
+        } finally {
+            try {
+                if (statement != null) statement.close();
+            } catch (SQLException e) {
+                System.out.println("Cannot close Statement. Machine error: "+e.toString());
+            }
+        }
     	System.out.println("----------------------------------------------------------------");
     }
 
